1dddccf73c240788ff0d0346d16da9a874ce60ba
arthurbarbosadornelassilva <xarthsilvax@gmail.com>
2025-07-14 20:31:43 -0300

    Correção do teste de 'n value repo mock'

--- tests/shared/infra/repositories/test_n_value_repository_mock.py
+++ tests/shared/infra/repositories/test_n_value_repository_mock.py
@@ -5,13 +5,13 @@ import pytest
 
 class Test_NValueRepositoryMock:
     def test_save_n_value(self):
-        n_value = N_Value(n_id=1, edl_prcnt=66.0, b_section=0.9, e_lux=200.0, e_external=20000.0, a_area=544.0, fd=0.7, cd_value=3.0)
+        n_value = N_Value(n_id=1, edl_prcnt=66.0, b_section=0.9, e_lux=200, e_external=20000.0, a_area=544.0, fd_value=0.7)
         repo = NValueRepositoryMock()
         saved_value = repo.save_edl_value(n_value)
         assert saved_value == n_value
 
     def test_get_n_value_by_id(self):
-        n_value = N_Value(n_id=1,  edl_prcnt=66.0, b_section=0.9, e_lux=200.0, e_external=20000.0, a_area=544.0, fd=0.7, cd_value=3.0)
+        n_value = N_Value(n_id=1,  edl_prcnt=66.0, b_section=0.9, e_lux=200, e_external=20000.0, a_area=544.0, fd_value=0.7)
         repo = NValueRepositoryMock(n_value)
         retrieved_value = repo.get_n_value_by_id(1)
         assert retrieved_value == n_value
