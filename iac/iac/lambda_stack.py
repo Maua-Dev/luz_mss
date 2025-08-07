740dc8fb9cd15cfdc8f36e833fdabe79ee5f5104
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-08-07 16:03:29 -0300

    🤡

--- iac/iac/lambda_stack.py
+++ iac/iac/lambda_stack.py
@@ -38,7 +38,7 @@ class LambdaStack(Construct):
                                                  compatible_runtimes=[lambda_.Runtime.PYTHON_3_9]
                                                  )
 
-        layer_arn = f"arn:aws:lambda:{environment_variables.get("REGION", "sa-east-1")}:017000801446:layer:AWSLambdaPowertoolsPythonV2:22"
+        layer_arn = f"arn:aws:lambda:{environment_variables.get('REGION', 'sa-east-1')}:017000801446:layer:AWSLambdaPowertoolsPythonV2:22"
         self.lambda_power_tools = lambda_.LayerVersion.from_layer_version_arn(self, "Lambda_Power_Tools", layer_version_arn=layer_arn)
 
         self.calculate_edl_value_function = self.create_lambda_api_gateway_integration(
