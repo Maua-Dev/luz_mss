0e2d5e5db277f54d0f1791ccd7c9af1a75974a08
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-08-07 15:40:37 -0300

    fixing import

--- iac/app.py
+++ iac/app.py
@@ -4,7 +4,7 @@ import os
 import aws_cdk as cdk
 from adjust_layer_directory import adjust_layer_directory
 
-from iac.iac.iac_stack import TemplateStack
+from iac.iac_stack import IacStack
 
 
 
@@ -40,7 +40,7 @@ tags = {
     'owner': 'DevCommunity'
 }
 
-TemplateStack(app, stack_name=stack_name, env=cdk.Environment(account=aws_account_id, region=aws_region), tags=tags)
+IacStack(app, stack_name=stack_name, env=cdk.Environment(account=aws_account_id, region=aws_region), tags=tags)
 
 
 app.synth()
